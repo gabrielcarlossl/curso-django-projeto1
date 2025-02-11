742c7f79b9d75d949efdecc26699de2fd8321424
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2025-02-11 09:59:57 -0300

    removed contact and about

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-def contato(request):
-    return HttpResponse('contanto!')
-  
-def sobre(request):
-    return HttpResponse('sobre!')
