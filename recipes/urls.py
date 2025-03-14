03a6db555af7ede648524b96c992fe2f2d9b5248
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2025-03-14 16:53:16 -0300

    feat: add dynamic url

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,7 +4,9 @@ from django.conf.urls.static import static
 
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
