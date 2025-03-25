0c547e281231659096b511c327bc38e4fffde2d5
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2025-03-24 21:43:47 -0300

    chore: setup initial tests

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+class RecipeUrlsTest(TestCase):
+  def test_the_pytest_is_ok(self):
+    print('test print')
+    var = '77'
+    print('testing...')
