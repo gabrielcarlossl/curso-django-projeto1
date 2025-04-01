798c5a9c2f785d114f8b1d3adc07389f4fe8425d
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2025-03-31 21:20:13 -0300

    test: add unit test for home, recipe category and recipe urls

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,7 +1,14 @@
 from django.test import TestCase
-
-class RecipeUrlsTest(TestCase):
-  def test_the_pytest_is_ok(self):
-    print('test print')
-    var = '77'
-    print('testing...')
+from django.urls import reverse
+class RecipeUrlsTest(TestCase):    
+  def test_recipe_home_url_is_correct(self):
+    url= reverse('recipes:home')
+    self.assertEqual(url, '/')
+  
+  def test_recipe_category_url_is_correct(self):
+    url= reverse('recipes:category', kwargs={'category_id': 1})
+    self.assertEqual(url, '/recipes/category/1/')
+    
+  def test_recipe_url_is_correct(self):
+    url= reverse('recipes:recipe', kwargs={'id': 1})
+    self.assertEqual(url, '/recipes/1/')
