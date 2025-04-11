46bc4ee865ecba6234ce967e292c56b4edf78c3a
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2025-04-11 10:29:19 -0300

    test: add checks for unpublished recipes in category and detail views

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -80,6 +80,16 @@ class RecipeViewsTest(RecipeTestBase):
     content = response.content.decode('utf-8')
     self.assertIn(recipe_title, content)
     
+  
+  def test_recipe_category_template_dont_load_recipes_not_published(self):
+    """
+    Test recipe is_published False don't is displayed in category page
+    """
+    
+    recipe = self.make_recipe(is_published=False)
+    response = self.client.get(reverse('recipes:recipe', kwargs={'id': recipe.category.id}))
+    self.assertEqual(response.status_code, 404)
+    
     
   def test_recipe_detail_view_function_is_correct(self):
     view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
@@ -90,4 +100,13 @@ class RecipeViewsTest(RecipeTestBase):
     response = self.client.get(
       reverse('recipes:recipe', kwargs={'id': 1000})
     )
+    self.assertEqual(response.status_code, 404)
+    
+  def test_recipe_detail_template_dont_load_recipe_not_published(self):
+    """
+    Test recipe is_published False don't is displayed in detail page
+    """
+    
+    recipe = self.make_recipe(is_published=False)
+    response = self.client.get(reverse('recipes:recipe', kwargs={'id': recipe.id}))
     self.assertEqual(response.status_code, 404)
